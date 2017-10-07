9142e07a7adf674b783b9057da43277eefb18525
sakda homhuan <sakda.homhuan@gmail.com>
2017-10-07 10:30:18 +0700

    update source

--- sql.sql
+++ sql.sql
@@ -1,9 +1,9 @@
-﻿CREATE TABLE feature
+CREATE TABLE public.feature
 (
-  gid serial NOT NULL,
+  gid integer NOT NULL DEFAULT nextval('feature_gid_seq'::regclass),
   name_t character varying(50),
   desc_t character varying(50),
-  typ_n smallint,
   geom geometry(Geometry,4326),
+  type_g text,
   CONSTRAINT addfeature_feature_pkey PRIMARY KEY (gid)
 )
