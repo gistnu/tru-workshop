80745e1d30e7209065ecfb0c062ffec1b24cacf2
aegistz <teerayoot5056@gmail.com>
2017-10-07 13:35:27 +0700

    13.35

--- geojson.php
+++ geojson.php
@@ -6,16 +6,16 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 
 pg_connect("host=localhost user=postgres password=1234 dbname=addfeature") or die("Can't Connect Server");
 $type = $_GET[type];
-$sql = "SELECT jsonb_build_object(
+$sql = "SELECT json_build_object(
     'type',     'FeatureCollection',
-    'features', jsonb_agg(feature)
+    'features', json_agg(feature)
 )
 FROM (
-  SELECT jsonb_build_object(
+  SELECT json_build_object(
     'type',       'Feature',
     'id',         gid,
-    'geometry',   ST_AsGeoJSON(geom)::jsonb,
-    'properties', to_jsonb(row) - 'gid' - 'geom'
+    'geometry',   ST_AsGeoJSON(geom)::json,
+    'properties', to_json(row)
   ) AS feature
   FROM (SELECT * FROM feature where type_g = '$type') row) features;";
 
